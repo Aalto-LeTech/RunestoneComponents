b0b8fa083066cda903ed4d91dcc2580144205e37
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2016-02-05 14:12:40 +0200

    Disabled built-in logging @ runestonebase

--- runestone/common/js/runestonebase.js
+++ runestone/common/js/runestonebase.js
@@ -3,18 +3,17 @@ function RunestoneBase () {   // Basic parent stuff
 }
 
 RunestoneBase.prototype.logBookEvent = function (eventInfo) {
-    eventInfo.course = eBookConfig.course;
-    if (eBookConfig.useRunestoneServices && eBookConfig.logLevel > 0) {
-        jQuery.get(eBookConfig.ajaxURL + 'hsblog', eventInfo); // Log the run event
-    }
-    console.log("logging event " + eventInfo);
+    // eventInfo.course = eBookConfig.course;
+    // if (eBookConfig.useRunestoneServices && eBookConfig.logLevel > 0) {
+    //     jQuery.get(eBookConfig.ajaxURL + 'hsblog', eventInfo); // Log the run event
+    // }
+    // console.log("logging event " + eventInfo);
 };
 
 RunestoneBase.prototype.logRunEvent = function (eventInfo) {
-    eventInfo.course = eBookConfig.course;
-    if (eBookConfig.useRunestoneServices && eBookConfig.logLevel > 0) {
-        jQuery.post(eBookConfig.ajaxURL + 'runlog', eventInfo); // Log the run event
-    }
-    console.log("running " + eventInfo);
+    // eventInfo.course = eBookConfig.course;
+    // if (eBookConfig.useRunestoneServices && eBookConfig.logLevel > 0) {
+    //     jQuery.post(eBookConfig.ajaxURL + 'runlog', eventInfo); // Log the run event
+    // }
+    // console.log("running " + eventInfo);
 };
-
